23e478931a10ab2a1b30c143f82bd18531c041f0
Naum.Bebekoski-FE18 <bebekoski123@gmail.com>
2024-09-28 12:43:14 +0200

    Nav Bar

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -18,8 +18,6 @@ const Navbar = () => {
     <div>
       <nav className="bg-white">
         <div className="mx-auto max-w-[173vh] px-2 sm:px-6 lg:px-8">
-          {/* <div className="flex-1 mx-auto"> */}
-
           <div className="relative flex h-16 items-center justify-between">
             {/* Logo on the left side */}
             <div className="flex items-center justify-start">
@@ -29,8 +27,27 @@ const Navbar = () => {
               </p>
             </div>
 
-            {/* Menu button for mobile view */}
-            <div className="absolute inset-y-0 left-[40vh] flex items-center sm:hidden">
+            {/* Navigation links on the right side */}
+            <div className="hidden sm:flex sm:items-center sm:ml-auto">
+              <div className="flex space-x-4 items-center">
+                {navigation.map((item) => (
+                  <div key={item.name}>
+                    <a
+                      href={item.href}
+                      className="text-sm font-quicksand text-black font-extrabold"
+                    >
+                      {item.name}
+                    </a>
+                  </div>
+                ))}
+                <div className="bg-custom_red text-white font-quicksand font-semibold pt-2 pb-2 pr-6 pl-6">
+                  <p>1800 789 123</p>
+                </div>
+              </div>
+            </div>
+
+            {/* Menu button for mobile view on the right side */}
+            <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
               <button
                 onClick={() => setIsOpen(!isOpen)}
                 className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-black hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
@@ -66,26 +83,6 @@ const Navbar = () => {
                   </svg>
                 )}
               </button>
-            </div>
-
-            {/* Navigation links on the right side */}
-            <div className="hidden sm:flex sm:items-center sm:ml-auto">
-              <div className="flex space-x-4 items-center">
-                {navigation.map((item) => (
-                  <div>
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="text-sm font-quicksand text-black font-extrabold"
-                    >
-                      {item.name}
-                    </a>
-                  </div>
-                ))}
-                <div className="bg-custom_red text-white font-quicksand font-semibold pt-2 pb-2 pr-6 pl-6">
-                  <p>1800 789 123</p>
-                </div>
-              </div>
             </div>
           </div>
         </div>
